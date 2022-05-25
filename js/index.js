9593594b059728edd8f7a99a0e40d1ba414db998
aSt0m <61126327+aSt0m@users.noreply.github.com>
2022-05-25 05:11:22 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+const menuDrop = document.querySelector(".menu-drop");
+const linkDown= document.querySelector(".linkDown");
+const visible = document.querySelector(".visible");
+
+
+linkDown.addEventListener('click',()=>{
+    menuDrop.classList.add('.active')
+
+})
