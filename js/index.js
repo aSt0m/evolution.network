7d0834c76d444131958d5ffed8d5be9b88c46d91
aSt0m <61126327+aSt0m@users.noreply.github.com>
2022-05-26 05:03:12 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,10 +1,59 @@
 
-const menuDrop = document.querySelector(".menu-drop");
-const linkDown= document.querySelector(".linkDown");
-const visible = document.querySelector(".visible");
+// const menuDrop = document.querySelector(".menu-drop");
+// const linkDown= document.querySelector(".linkDown");
+// const visible = document.querySelector(".visible");
 
 
-linkDown.addEventListener('click',()=>{
-    menuDrop.classList.add('.active')
+// linkDown.addEventListener('click',()=>{
+//     menuDrop.classList.add('.active')
 
-})
+// })
+
+
+// /** carrousel */
+// var swiper = new Swiper(".mySwiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "2",
+//     coverflowEffect: {
+//       rotate: 0,
+//       stretch: 0,
+//       depth: 300,
+//       modifier: 2,
+//       slideShadows: true,
+//     },
+//     autoplay: {
+//       delay: 5000,
+//     },
+//    loop: true,
+//   });
+  
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: "3",
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    autoplay: {
+               delay: 5000,
+             },
+  });
+  
