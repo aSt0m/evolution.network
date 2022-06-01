b53a209d3e7570167fdaa16a26fb1f9b3e2c4305
aSt0m-- <gavomaza1@gmail.com>
2022-06-01 17:16:13 -0500

    Carrousel header

--- js/index.js
+++ js/index.js
@@ -10,69 +10,89 @@
 // })
 
 
-// /** carrousel */
-// var swiper = new Swiper(".mySwiper", {
-//     effect: "coverflow",
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: "2",
-//     coverflowEffect: {
-//       rotate: 0,
-//       stretch: 0,
-//       depth: 300,
-//       modifier: 2,
-//       slideShadows: true,
-//     },
-//     autoplay: {
-//       delay: 5000,
-//     },
-//    loop: true,
-//   });
+/** carrousel header */
+var swipeHeader = new Swiper(".swiper-header", {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  slidesPerView: "1",
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+   
   
+  }
+});
+
 
 //**Carrousel */
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: "3",
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: "1",
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    autoplay: {
-               delay: 5000,
-             },
+  }
+});
+
+
+/**Hamburguer menu */
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.nav-down-right');
+const barras = document.querySelectorAll('.ham span');
+
+
+
+
+ham.addEventListener('click', () => {
+  enlaces.classList.toggle('activado');
+  barras.forEach(child => {
+    child.classList.toggle('animado')
   });
-  
+  ham.classList.toggle('girar');
 
-   /**Hamburguer menu */
- const ham = document.querySelector('.ham');
- const enlaces = document.querySelector('.nav-down-right');
- const barras = document.querySelectorAll('.ham span');
-
-
-
-
- ham.addEventListener('click', () => {
-     enlaces.classList.toggle('activado');
-     barras.forEach(child => {
-         child.classList.toggle('animado')});
-     ham.classList.toggle('girar');  
-     
- });
+});
 
 
 
