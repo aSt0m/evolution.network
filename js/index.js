96aafa5a9b0121668770cd5e2c30b0141e2fd9c2
aSt0m-- <gavomaza1@gmail.com>
2022-06-03 03:21:01 -0500

    page-inner

--- js/index.js
+++ js/index.js
@@ -18,20 +18,20 @@ var swipeHeader = new Swiper(".swiper-header", {
   slidesPerView: "1",
 
   // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
 
   // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 
   // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
   autoplay: {
     delay: 5000,
   },
